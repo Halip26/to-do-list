5bdffff2977d798b0af9e794e4b3302fb4a0c647
Halip26 <halipuddin.angko@gmail.com>
2023-08-07 21:10:16 +0800

    update all the codes

--- script.js
+++ script.js
@@ -25,33 +25,48 @@ function showTodo(filter) {
       let completed = todo.status == "completed" ? "checked" : "";
       if (filter == todo.status || filter == "all") {
         liTag += `<li class="task">
-                            <label for="${id}">
-                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                                <p class="${completed}">${todo.name}</p>
-                            </label>
-                            <div class="settings">
-                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                <ul class="task-menu">
-                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
-                                </ul>
-                            </div>
-                        </li>`;
+                    <label for="${id}">
+                        <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                        <p class="${completed}">${todo.name}</p>
+                    </label>
+                    <div class="settings">
+                        <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                        <ul class="task-menu">
+                            <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                            <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                        </ul>
+                    </div>
+                </li>`;
       }
     });
   }
+  // Set the innerHTML of taskBox to either liTag or a message indicating no tasks
   taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+
+  // Check if there are any tasks present
   let checkTask = taskBox.querySelectorAll(".task");
-  !checkTask.length
-    ? clearAll.classList.remove("active")
-    : clearAll.classList.add("active");
-  taskBox.offsetHeight >= 265
-    ? taskBox.classList.add("overflow")
-    : taskBox.classList.remove("overflow");
+  if (!checkTask.length) {
+    // If no tasks, remove active class from clearAll button
+    clearAll.classList.remove("active");
+  } else {
+    // If tasks present, add active class to clearAll button
+    clearAll.classList.add("active");
+  }
+
+  // Check if the height of taskBox is more than or equal to 265
+  if (taskBox.offsetHeight >= 265) {
+    // If height is more, add overflow class to taskBox
+    taskBox.classList.add("overflow");
+  } else {
+    // If height is less, remove overflow class from taskBox
+    taskBox.classList.remove("overflow");
+  }
 }
 // to show all the lists
 showTodo("all");
 
+/* showMenu is responsible for displaying 
+a menu associated with a selected task.*/
 function showMenu(selectedTask) {
   let menuDiv = selectedTask.parentElement.lastElementChild;
   menuDiv.classList.add("show");
@@ -62,8 +77,10 @@ function showMenu(selectedTask) {
   });
 }
 
+// This function updates the status of a selected task
 function updateStatus(selectedTask) {
   let taskName = selectedTask.parentElement.lastElementChild;
+  // Check if there are any tasks present
   if (selectedTask.checked) {
     taskName.classList.add("checked");
     todos[selectedTask.id].status = "completed";
@@ -71,6 +88,7 @@ function updateStatus(selectedTask) {
     taskName.classList.remove("checked");
     todos[selectedTask.id].status = "pending";
   }
+  // Update the localStorage with the updated "todos" array
   localStorage.setItem("todo-list", JSON.stringify(todos));
 }
 
